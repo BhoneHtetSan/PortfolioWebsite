d5b4e8147f81270359ca5ecb9331796fd921470a
Bhone Htet San <bhoneh1@gmail.com>
2025-03-12 22:14:24 +0630

    Hover on video fix and some text adjustments

--- js/ie8.js
+++ js/ie8.js
@@ -2,3 +2,19 @@ jQuery(document).ready(function(){
 	"use strict";
     jQuery('body').html('<div class="ie8"><span>Hi! Please, use the latest version of the browser in order to visit current page</span></div>');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const iframes = document.querySelectorAll("iframe");
+
+    iframes.forEach((iframe) => {
+        iframe.addEventListener("load", function () {
+            iframe.addEventListener("mouseenter", function () {
+                document.body.classList.add("disable-cursor-effect");
+            });
+
+            iframe.addEventListener("mouseleave", function () {
+                document.body.classList.remove("disable-cursor-effect");
+            });
+        });
+    });
+});
